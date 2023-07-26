e4eb9169e03fc05819f9a02d0c93aea58581aa27
galgavrilenko <ggalkka@gmail.com>
2023-07-26 09:21:08 +0300

    test URLS5

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 app_name = 'api'
 
 urlpatterns = [
-    path('api/', include('api.users.urls')),
-    path('api/', include('api.recipes.urls')),
+    path('', include('api.users.urls')),
+    path('', include('api.recipes.urls')),
 ]
