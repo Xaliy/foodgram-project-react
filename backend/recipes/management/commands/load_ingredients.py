1c7f0ffd081fc9b4606e4fe8157b63a0a001663f
galgavrilenko <ggalkka@gmail.com>
2023-07-26 13:01:27 +0300

    Урлс, перенесла папку для миграции , изменила путь до файла в загрузочном файле

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -5,15 +5,17 @@ from django.core.management import BaseCommand
 
 from recipes.models import Ingredient
 
-FILES_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
+# FILES_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
 
 
 class Command(BaseCommand):
     help = "import data from ingredients.csv"
 
     def handle(self, *args, **kwargs):
-        with open(f'{FILES_DIR}/foodgram-project-react/data/ingredients.csv',
-                  'r', encoding='utf-8') as file:
+        # with open(f'{FILES_DIR}/foodgram-project-react/data/ingredients.csv',
+        #           'r', encoding='utf-8') as file:
+        with open('recipes/data/ingredients.csv',
+                  'r', encoding='utf-8') as file:        
             file_reader = csv.reader(file)
             next(file_reader)
 
